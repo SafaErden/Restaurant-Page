404ca183fe845f306a845b38841d9ffb962774e7
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-08-24 17:55:16 +0300

    create navbar with only JS

---
+++ src/header.js
@@ -0,0 +1,33 @@
+const createNav = () => {
+	var mainTitle = document.createElement('h1');
+	mainTitle.classList.add('p-0', 'display-3');
+	var titleContent = document.createElement('kbd');
+	titleContent.innerText = "The Safa's";
+	mainTitle.appendChild(titleContent);
+
+	var nav = document.createElement('nav');
+	nav.classList.add('mt-5');
+
+	var menuTabs = document.createElement('div');
+	menuTabs.id = 'nav-tab';
+	menuTabs.classList.add('nav', 'nav-tabs');
+
+	const tabs = [ 'Home', 'Menu', 'Contact' ];
+
+	tabs.forEach((item, index) => {
+		var tabItem = document.createElement('a');
+		tabItem.id = '#nav-' + item.toLowerCase() + '-tab';
+		tabItem.href = '#nav-' + item.toLowerCase();
+		tabItem.classList.add('nav-item', 'nav-link', 'font-weight-bold', 'h3');
+		if (index === 0) {
+			tabItem.classList.add('active');
+		}
+		tabItem.innerText = item;
+		menuTabs.appendChild(tabItem);
+	});
+
+	nav.appendChild(menuTabs);
+
+	return [ mainTitle, nav ];
+};
+export default createNav;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-//alert('safa erden');
+import createNav from './header.js';
+
+const content = document.getElementById('content');
+
+const navbar = createNav()[0];
+const navMenu = createNav()[1];
+
+content.appendChild(navbar);
+content.appendChild(navMenu);
